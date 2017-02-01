c90801478c9bbcac0620ed2d7bab2ae6889e7635
Dave Tarrant <davetaz@theodi.org>
2017-02-01 08:17:45 +0000

    Adding eLearning course

--- js/people.js
+++ js/people.js
@@ -4,7 +4,7 @@ function getPeople(type,destination) {
             people = data[type];
             for (r=0;r<people.length;r++) {
                 person = people[r];
-                outputPerson(person,destination);
+                getPersonData(person,destination);
             }
         })
         .error(function() {
@@ -12,16 +12,39 @@ function getPeople(type,destination) {
         });
 }
 
-function outputPerson(person,destination) {
+function getPersonData(person,destination) {
+    console.log(person);
+     $.getJSON( "http://contentapi.theodi.org/"+person["name"]+".json")
+            .done(function(data) {
+                        name = data.title;
+                        console.log(name);
+                        role = person.specialism;
+                        if (!person.specialism) role = data.details.role;
+                        image_src="//static.theodi.org/assets/training/avatar.gif";
+                        if (data.details.image) image_src = data.details.image.versions.square;
+                        outputPerson(destination,name,role,image_src);
+            })
+            .fail(function() {
+                        console.log('fail');
+                        image_src="//static.theodi.org/assets/training/avatar.gif";
+                        name = person.name.replace("-"," ");
+                        name = name.capitalize();
+                        console.log(name);
+                        role = person.specialism;
+                        outputPerson(destination,name,role,image_src);
+            });
+}
+
+function outputPerson(destination,name,role,image_src) {
     block = '';
     block += '                <div class="col-xs-12 col-sm-3">';
     block += '                    <div class="single_teacher_profile">';
     block += '                        <div class="teacher_thumb">';
-    block += '                            <img src="img/teacher-img/team-member-1.jpg" alt="">';             
+    block += '                            <img src="'+image_src+'" alt="">';             
     block += '                        </div>';
     block += '                        <div class="single_teacher_details_info">';
-    block += '                            <h5>'+person["name"]+'</h5>';
-    block += '                            <p>'+person["specialism"]+'</p>';
+    block += '                            <h5>'+name+'</h5>';
+    block += '                            <p>'+role+'</p>';
     block += '                        </div>';
     block += '                    </div>';
     block += '                </div>';
