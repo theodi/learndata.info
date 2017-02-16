9c6c46af47a216c308e6f1e4f246de7037bf3689
Dave Tarrant <davetaz@theodi.org>
2017-02-16 08:38:08 +0000

    Yay

--- js/action.js
+++ js/action.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    addCourses();
     processListeners();
 });
 
@@ -49,4 +50,41 @@ function processListeners() {
       		}
 	  	}
 	);
+}
+
+function addCourses() {
+	renderCourse('Open Data in a day','courses_box','explorer','odd');
+	renderCourse('Practical data management','courses_box','practitioner','odm');
+	renderCourse('Open data publication','courses_box','practitioner','odpub');
+ 	renderCourse('Visualising open data','courses_box','practitioner','odv'); 
+    renderCourse('Open data science','courses_box','practitioner','ods');  
+  	renderCourse('Building success in open data','courses_box','strategist','bs');
+  	renderCourse('Managing risk in open data','courses_box','strategist','mr');
+  	renderCourse('Business innovation workshop','courses_box','strategist','biw');
+    renderCourse('Practical leadership in open data','courses_box','pioneer','plod'); 
+    renderCourse('Data driven decisions','courses_box','pioneer','odpol');
+	
+	renderCourse('What is open data?','elearning_box','explorer','mod1');
+	renderCourse('Unlocking value from open data','elearning_box','explorer','mod2');
+	renderCourse('Open data. Agent of change.','elearning_box','strategist','mod3');
+	renderCourse('Why do we need to license?','elearning_box','explorer','mod4');
+	renderCourse('What makes quality open data?','elearning_box','practitioner','mod5');
+	renderCourse('Measureing success for open data','elearning_box','strategist','mod6');
+	renderCourse('Why should we worry about sustainability?','elearning_box','explorer','mod7');
+	renderCourse('Geting to grips with platforms','elearning_box','practitioner','mod8');
+	renderCourse('Choosing the right format for open data','elearning_box','practitioner','mod9');
+	renderCourse('How useful is my data?','elearning_box','strategist','mod10');
+	renderCourse('How to clean your data','elearning_box','practitioner','mod11');
+	renderCourse('Finding hidden data on the web','elearning_box','practitioner','mod12');
+	renderCourse('Linking up the web of data','elearning_box','practitioner','mod13');
+
+}
+
+function renderCourse(title,element,framework,id) {
+    block =  '<div class="'+framework+' price_rating_area course_filter" style="padding: 5px 5px 2px; margin-bottom: 6px;" id="'+id+'">';
+    block += '   <div class="price">';
+    block += '      <span style="font-size: 16px; font-weight: 500;">'+title+'</span>';
+    block += '   </div>';
+    block += '</div>';
+    $("#"+element).append(block);
 }
